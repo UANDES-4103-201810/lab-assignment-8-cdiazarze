a5bc73d82040c0a15fd413428fd67fa970c4672e
cdiazarze <cdiaz1@miuandes.cl>
2018-05-09 19:36:07 -0300

    Added bootstrap layout and partial for user index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,38 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
 
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = @user.products
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    @product=@user.products.find(params[:id])
   end
 
   # GET /products/new
   def new
-    @product = Product.new
+    @user=User.find(params[:user_id])
+    @product = @user.products.new
   end
 
   # GET /products/1/edit
   def edit
+    @product=@user.products.find(params[:id])
   end
 
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @user=User.find(params[:user_id])
+    @product = @user.products.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to user_products_path(@user), notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -40,9 +44,10 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
+    @product=@user.products.find(params[:id])
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to user_products_path(@user), notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -54,17 +59,18 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    @product=@user.products.find(params[:id])
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to user_products_path(@user), notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
+    def set_user
+      @user = User.find(params[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module LabAssignment8Cdiazarze
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    console do
+      ActiveRecord::Base.connection
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
